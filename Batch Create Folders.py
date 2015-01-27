51b552e18b9e078b70ec79d6fef145bf2bc40d24
iankim87 <iankim87@gmail.com>
2015-01-26 18:54:53 -0800

    second commit

--- Batch Create Folders.py
+++ Batch Create Folders.py
@@ -1,9 +1,6 @@
 import os, csv
 
-#source_dir = r"R:\For_Ian\OQ2_Final"
 destination_dir = r"R:\For_Ian\v2.0_Workstation\Lumen_Variability\v2.0\v1.4_OQ"
-#filename = "FFRData.txt"
-#filename = input("Enter the name of the file you would like to copy")
 
 # Go to active directory
 os.chdir(destination_dir)
@@ -14,8 +11,6 @@ for row in data:
     print row
     value1 = row[0]
     print value1
-    #value2 = row[1]
-    #print value2
 
     if os.path.exists(os.path.join(destination_dir, value1)):
         continue
