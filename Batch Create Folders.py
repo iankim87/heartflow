0dcb814c4dbd435aa764450b322fd8f24c0e63a2
iankim87 <iankim87@gmail.com>
2015-01-26 18:36:36 -0800

    Add first file

---
+++ Batch Create Folders.py
@@ -0,0 +1,23 @@
+import os, csv
+
+#source_dir = r"R:\For_Ian\OQ2_Final"
+destination_dir = r"R:\For_Ian\v2.0_Workstation\Lumen_Variability\v2.0\v1.4_OQ"
+#filename = "FFRData.txt"
+#filename = input("Enter the name of the file you would like to copy")
+
+# Go to active directory
+os.chdir(destination_dir)
+
+data = csv.reader(open("list.csv","rb"), delimiter=",")
+
+for row in data:
+    print row
+    value1 = row[0]
+    print value1
+    #value2 = row[1]
+    #print value2
+
+    if os.path.exists(os.path.join(destination_dir, value1)):
+        continue
+
+    os.mkdir(os.path.join(destination_dir, value1))
